5d495af56933f1b269d1a4ee38136d4a3c70b207
lukaszb <lukaszbomba@gmail.com>
2023-02-12 15:54:38 +0100

    Task 23

--- src/main/java/com/sda/service/UsersService.java
+++ src/main/java/com/sda/service/UsersService.java
@@ -1,8 +1,12 @@
 package com.sda.service;
 
 import com.sda.dao.UsersDAO;
+import com.sda.dto.UserDTO;
 import com.sda.mapper.UserMapper;
 import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 
@@ -10,4 +14,20 @@ public class UsersService {
 
     private final UsersDAO usersDAO;
     private final UserMapper usersMapper;
+
+    public List<UserDTO> findAll(){
+        // List<User> users = usersDAO.findAll();
+        // List<UserDTO> userDTOS = new ArrayList<>();
+
+        //for (User user : users){
+       //     UserDTO dto = usersMapper.map(user);
+       //     userDTOS.add(dto);
+       // }
+       // return userDTOS;
+
+        return usersDAO.findAll().stream()
+                .map(user -> usersMapper.map(user))
+                .toList();
+
+    }
 }
