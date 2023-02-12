226d0d529f6977eb2c4c19b4bebc399cc1697de8
Magdalena Stachowiak <stachowiak.mg@gmail.com>
2023-02-12 11:25:14 +0100

    user entity added

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -1,13 +1,18 @@
 package com.sda;
 
+import com.sda.dao.UsersDAO;
+import com.sda.db.HibernateUtils;
+import com.sda.model.User;
+import org.hibernate.Session;
+
 public class Main {
 
     public static void main(String[] args) {
+        UsersDAO usersDAO = new UsersDAO();
 
+        User user = new User();
+        user.setUsername("root");
 
-
-
-
+        usersDAO.create(user);
     }
-
 }
