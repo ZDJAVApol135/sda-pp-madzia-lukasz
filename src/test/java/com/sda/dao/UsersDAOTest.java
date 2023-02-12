01cd55e3ab5f471446d783c0997d44fce9d2d489
lukaszb <lukaszbomba@gmail.com>
2023-02-12 13:52:25 +0100

    Task 16

--- src/test/java/com/sda/dao/UsersDAOTest.java
+++ src/test/java/com/sda/dao/UsersDAOTest.java
@@ -1,12 +1,15 @@
 package com.sda.dao;
 
-
+import com.github.javafaker.Faker;
+import com.github.javafaker.Internet;
+import com.github.javafaker.Name;
 import com.sda.db.HibernateUtils;
 import com.sda.model.User;
 import org.hibernate.Session;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.UUID;
 
 public class UsersDAOTest {
@@ -15,24 +18,17 @@ public class UsersDAOTest {
 
     @Test
     void testCreateHappyPath() {
-        //given
+        // given
         String username = UUID.randomUUID().toString();
-        User expectedUser = new User();
-        expectedUser.setUsername(username);
-        expectedUser.setAge(28);
-        expectedUser.setName("Karen");
-        expectedUser.setSurname("Smith");
-        expectedUser.setEmail("karen123@gmail.com");
+        User expectedUser = createUser(username);
 
-        //when
+        // when
         usersDAO.create(expectedUser);
-        //then
+
+        // then
         Session session = HibernateUtils.openSession();
         User actualUser = session.find(User.class, username);
         session.close();
-
-        Assertions.assertNotNull(actualUser);
-        Assertions.assertEquals(expectedUser, actualUser);
 
         Assertions.assertNotNull(actualUser);
         Assertions.assertEquals(expectedUser, actualUser);
@@ -46,34 +42,84 @@ public class UsersDAOTest {
 
     @Test
     void testDeleteUserNotExist() {
-        //given
-        String notExistedUser = "User not exists";
+        // given
+        String nonExistingUsername = "NON EXISTING USER";
 
-        //when
-        boolean delete = usersDAO.delete(notExistedUser);
+        // when
+        boolean deleted = usersDAO.delete(nonExistingUsername);
 
-        //then
-        Assertions.assertFalse(delete);
+        // then
+        Assertions.assertFalse(deleted);
     }
+
     @Test
     void testDeleteUserExist() {
-        //given
-        String username = UUID.randomUUID().toString();
-        User expectedUser = new User();
-        expectedUser.setUsername(username);
-        expectedUser.setAge(28);
-        expectedUser.setName("Karen");
-        expectedUser.setSurname("Smith");
-        expectedUser.setEmail("karen123@gmail.com");
+        // given
+        String username = "username";
+        User expectedUser = createUser(username);
 
         usersDAO.create(expectedUser);
 
-        //when
-        boolean delete = usersDAO.delete(username);
+        // when
+        boolean deleted = usersDAO.delete(username);
 
-        //then
-        Assertions.assertTrue(delete);
+        // then
+        Assertions.assertTrue(deleted);
     }
 
-}
 
+    @Test
+    void testFindAll() {
+        // given
+        final List<User> expectedList = List.of(
+                createUser(UUID.randomUUID().toString()),
+                createUser(UUID.randomUUID().toString()),
+                createUser(UUID.randomUUID().toString())
+        );
+
+        expectedList.forEach(usersDAO::create);
+
+        // when
+        List<User> usersList = usersDAO.findAll();
+
+        // then
+        Assertions.assertNotNull(usersList);
+        Assertions.assertEquals(expectedList.size(), usersList.size());
+        Assertions.assertIterableEquals(expectedList, usersList);
+    }
+
+    @Test
+    void testFindByUsername() {
+        // given
+        String username = UUID.randomUUID().toString();
+        User expectedUser = createUser(username);
+        usersDAO.create(expectedUser);
+
+        // when
+        User actualUser = usersDAO.findByUsername(username);
+
+        // then
+        Assertions.assertNotNull(actualUser);
+        Assertions.assertEquals(expectedUser, actualUser);
+        Assertions.assertEquals(expectedUser.getAge(), actualUser.getAge());
+        Assertions.assertEquals(expectedUser.getEmail(), actualUser.getEmail());
+        Assertions.assertEquals(expectedUser.getName(), actualUser.getName());
+        Assertions.assertEquals(expectedUser.getPassword(), actualUser.getPassword());
+        Assertions.assertEquals(expectedUser.getSurname(), actualUser.getSurname());
+    }
+
+    private User createUser(String username) {
+        Faker faker = new Faker();
+        Name name = faker.name();
+        Internet internet = faker.internet();
+
+        User user = new User();
+        user.setUsername(username);
+        user.setName(name.firstName());
+        user.setSurname(name.lastName());
+        user.setPassword(internet.password());
+        user.setEmail(internet.emailAddress());
+        user.setAge(faker.number().numberBetween(1, 150));
+        return user;
+    }
+}
