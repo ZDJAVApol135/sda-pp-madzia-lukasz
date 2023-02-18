fad633933f5c15e51872821e5f07a6d37105c867
lukaszb <lukaszbomba@gmail.com>
2023-02-18 12:09:37 +0100

    prework

---
+++ src/main/java/com/sda/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.sda.controller;
+
+import com.sda.service.UsersService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserController {
+   private final UsersService usersService;
+}
