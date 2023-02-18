e160190ed807b775e7df68b9558844a832584697
lukaszb <lukaszbomba@gmail.com>
2023-02-18 09:42:29 +0100

    Task 23

---
+++ src/main/java/com/sda/mapper/UserMapper 2.java
@@ -0,0 +1,16 @@
+package com.sda.mapper;
+
+import com.sda.dto.UserDTO;
+import com.sda.model.User;
+
+public class UserMapper {
+    public UserDTO map(User User){
+        return UserDTO.builder()
+                .username(User.getUsername())
+                .name(User.getName())
+                .surname(User.getSurname())
+                .age(User.getAge())
+                .email(User.getEmail())
+                .build();
+    }
+}
