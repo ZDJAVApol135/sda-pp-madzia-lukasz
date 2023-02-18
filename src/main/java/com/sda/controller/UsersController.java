4d7ad013a003188db1be016f725cdf4225a01aa8
lukaszb <lukaszbomba@gmail.com>
2023-02-18 12:50:04 +0100

    Task29

--- src/main/java/com/sda/controller/UsersController.java
+++ src/main/java/com/sda/controller/UsersController.java
@@ -1,13 +1,22 @@
 package com.sda.controller;
 
 import com.sda.dto.UserDTO;
+import com.sda.exception.NotFoundException;
 import com.sda.service.UsersService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
+
+@Slf4j
 @RequiredArgsConstructor
 public class UsersController {
+
+//    private static final Logger log = LoggerFactory.getLogger(UsersController.class);
+
    private final UsersService usersService;
 
    public void findAll() {
@@ -18,6 +27,17 @@ public class UsersController {
       } else {
          System.out.println("Users list:");
          users.forEach(user -> System.out.println(user));
+//            users.forEach(System.out::println);
       }
    }
+
+   public void findByUsername(String username) {
+      try {
+         UserDTO userDTO = usersService.findByUsername(username);
+         System.out.printf("User found: %s%n", userDTO);
+      } catch (NotFoundException ex) {
+         log.error("NotFoundException: {}", ex.getMessage());
+      }
+   }
+
 }
